f0d01f502ccfa66c437687e5e3323cb49533f9f0
Adrian Petrosanu <adrian_sabin@yahoo.com>
2014-06-29 20:57:52 +0300

    no message

--- src/SendMain/SendMain.cpp
+++ src/SendMain/SendMain.cpp
@@ -1,6 +1,8 @@
 #include <GL/glew.h> // include GLEW and new version of GL on Windows
 #include <GLFW/glfw3.h> // GLFW helper library
 #include <stdio.h>
+#include <math.h>
+#define PI 3.14159265359
 
 // functie banala de incarcat continutul unui fisier intr-un buffer
 char * LoadFileInMemory(const char *filename)
@@ -23,6 +25,18 @@ char * LoadFileInMemory(const char *filename)
 	fclose(f);
 	buffer[size] = 0;
 	return buffer;
+}
+
+void pointAtAngle(float vec[], int i, float r, int u)
+{
+	float pi = atan(1.0f) * 4;
+	float urad = u * pi / 180.0f;
+	float x = r * cos(urad);
+	float y = r * sin(urad);
+	float z = 0.0f;
+	vec[3 * i] = x;
+	vec[3 * i + 1] = y;
+	vec[3 * i + 2] = z;
 }
 
 
@@ -54,7 +68,8 @@ int main() {
 	printf("OpenGL version supported %s\n", version);
 
 	const char * vertex_shader = LoadFileInMemory("../data/vertexShader.glsl");
-	const char * fragment_shader = LoadFileInMemory("../data/fragmentShader.glsl");
+	const char * fragment_shader = LoadFileInMemory("../data/pixelShader.glsl");
+	const char * fragment_shader2 = LoadFileInMemory("../data/pixelShader2.glsl");
 
 	GLuint vs = glCreateShader(GL_VERTEX_SHADER);
 	glShaderSource(vs, 1, &vertex_shader, NULL);
@@ -62,27 +77,83 @@ int main() {
 	GLuint fs = glCreateShader(GL_FRAGMENT_SHADER);
 	glShaderSource(fs, 1, &fragment_shader, NULL);
 	glCompileShader(fs);
+	GLuint fs2 = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fs2, 1, &fragment_shader2, NULL);
+	glCompileShader(fs2);
+
 	GLuint shader_programme = glCreateProgram();
 	glAttachShader(shader_programme, fs);
 	glAttachShader(shader_programme, vs);
 	glLinkProgram(shader_programme);
+	GLuint shader_programme2 = glCreateProgram();
+	glAttachShader(shader_programme2, fs2);
+	glAttachShader(shader_programme2, vs);
+	glLinkProgram(shader_programme2);
 
 	delete[] vertex_shader;
 	delete[] fragment_shader;
 
 	// buffer cu vertecsi in RAM 
-	float vertex_buffer[] = {
-		0.0f, 0.5f, 0.0f,
-		0.5f, -0.5f, 0.0f,
-		-0.5f, -0.5f, 0.0f
+	float vertex_buffer_triangle[] = {
+		0.5f, 0.15f, 0.0f,
+		0.6f, -0.05f, 0.0f,
+		0.4f, -0.05f, 0.0f
 	};
+
+	float vertex_buffer_triangle2[] = {
+		-0.5f, 0.15f, 0.0f,
+		-0.6f, -0.05f, 0.0f,
+		-0.4f, -0.05f, 0.0f
+	};
+
+	float vertex_buffer_square[] = {
+		0.1f, 0.6f, 0.0f,
+		0.1f, 0.4f, 0.0f,
+		-0.1f, 0.4f, 0.0f,
+		-0.1f, 0.6f, 0.0f,
+		0.1f, 0.6f, 0.0f,
+		-0.1f, 0.4f, 0.0f
+	};
+
+	float radius = 0.2f;
+	int number = 16;
+	float vertex_buffer_circle[600];
+	printf("Introduceti numarul de puncte cerc(max ~60):\nnumber=");
+	scanf("%d", &number);
+	int i = 0; // numar vertecsi total cerc
+	int j = 0; // numar vertecsi contur cerc
+	float step = 360.0f / (number + 0.0f);
+	float u = 0.0f;
+	while (j < 2 * number)
+	{
+		if (i % 3 == 2)
+		{
+			//fiecare al treilea punct este centrul
+			vertex_buffer_circle[3 * i] = vertex_buffer_circle[3 * i + 1] = vertex_buffer_circle[3 * i + 2] = 0.0f;
+		}
+		else
+		{
+			if (i % 3 == 0)
+			{
+				pointAtAngle(vertex_buffer_circle, i, radius, u);
+			}
+			else
+			{
+				u += step;
+				pointAtAngle(vertex_buffer_circle, i, radius, u);
+			}
+			j++;
+		}
+		i++;
+	}
+	vertex_buffer_circle[3 * i] = vertex_buffer_circle[3 * i + 1] = vertex_buffer_circle[3 * i + 2] = 0.0f;
+	i++;
 
 	// Generam un buffer in memoria video si scriem in el punctele din ram
 	GLuint vbo = 0;
 	glGenBuffers(1, &vbo); // generam un buffer 
 	glBindBuffer(GL_ARRAY_BUFFER, vbo); // setam bufferul generat ca bufferul curent 
-	glBufferData(GL_ARRAY_BUFFER, 9 * sizeof (float), vertex_buffer, GL_STATIC_DRAW); //  scriem in bufferul din memoria video informatia din bufferul din memoria RAM
-
+	
 	// De partea aceasta am uitat sa va spun la curs -> Pentru a defini bufferul alocat de opengl ca fiind buffer de in de atribute, stream de vertecsi trebuie sa :
 	// 1. Ii spunem OpenGL-ului ca vom avea un slot pentru acest atribut (in cazul nostru 0) , daca mai aveam vreun atribut ar fi trebuit si acela enablat pe alt slot (de exemplu 1)
 	// 2. Definit bufferul ca Vertex Attribute Pointer cu glVertexAttribPointer
@@ -97,8 +168,27 @@ int main() {
 		glUseProgram(shader_programme);
 		// facem bind la vertex buffer
 		glBindBuffer(GL_ARRAY_BUFFER, vbo);
+
+		//triunghi
+		//  scriem in bufferul din memoria video informatia din bufferul din memoria RAM
+		glBufferData(GL_ARRAY_BUFFER, 9 * sizeof (float), vertex_buffer_triangle, GL_STATIC_DRAW);
 		// draw points 0-3 from the currently bound VAO with current in-use shader
 		glDrawArrays(GL_TRIANGLES, 0, 3);
+
+		//patrat
+		glBufferData(GL_ARRAY_BUFFER, 18 * sizeof (float), vertex_buffer_square, GL_STATIC_DRAW);
+		glDrawArrays(GL_TRIANGLES, 0, 6);
+
+		//cerc
+		glBufferData(GL_ARRAY_BUFFER, 3 * i * sizeof (float), vertex_buffer_circle, GL_STATIC_DRAW);
+		glDrawArrays(GL_TRIANGLES, 0, i);
+
+		//galben
+		glUseProgram(shader_programme2);
+		//triunghi 2
+		glBufferData(GL_ARRAY_BUFFER, 9 * sizeof (float), vertex_buffer_triangle2, GL_STATIC_DRAW);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+
 		// facem swap la buffere (Double buffer)
 		glfwSwapBuffers(window);
 
